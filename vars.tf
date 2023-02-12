85f878723e06f94fe3c92d2c403cb6cc2850de77
Riley Snyder <rileysnyder@protonmail.ch>
2023-02-12 13:47:38 -0600

    feat: logic

--- vars.tf
+++ vars.tf
@@ -12,3 +12,15 @@ variable "url" {
   description = "The full URL to redirect to. Schema included."
   type        = string
 }
+
+variable "permanent" {
+  description = "Redirects with a 301 status code (defaults to 302 otherwise)"
+  type        = bool
+  default     = false
+}
+
+variable "path" {
+  description = "Path to match on the hostname"
+  type        = string
+  default     = ""
+}
